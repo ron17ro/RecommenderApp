ebc1ff7ae683354ed10df687780d2909a313825e
Roxana <ron17ro@yahoo.com>
2018-11-28 22:06:50 +0000

    added image placeholders for thumbnails

--- MainProgram.py
+++ MainProgram.py
@@ -1,13 +1,15 @@
 from flask import Flask, render_template, request 
-from location import my_location
+from location import *
+from places import *
 from settings import KEYS
+
 
 app = Flask("MyApp")
 
 @app.route("/")
 def display_location():
-	return render_template("index.html", location = my_location(), OpenWeatherMap_API_KEY = KEYS['OpenWeatherMap'] )
-
+    
+    return render_template("index.html", location = my_city(), OpenWeatherMap_API_KEY = KEYS['OpenWeatherMap'], all_places_display_name = all_places_names.values())
 
 
 if __name__ == '__main__':
--- location.py
+++ location.py
@@ -2,17 +2,29 @@ import re
 import json
 from urllib.request import urlopen
 
+
+url = 'http://ipinfo.io/json'
+response = urlopen(url)
+data = json.load(response)
+
+IP=data['ip']
+org=data['org']
+city = data['city']
+country=data['country']
+region=data['region']
+loc = data['loc']
+postal=data['postal']
+
 def my_location():
-    url = 'http://ipinfo.io/json'
-    response = urlopen(url)
-    data = json.load(response)
-
-    IP=data['ip']
-    org=data['org']
-    city = data['city']
-    country=data['country']
-    region=data['region']
-
     print('Your IP detail\n ')
     print('IP : {4} \nRegion : {1} \nCountry : {2} \nCity : {3} \nOrg : {0}'.format(org,region,country,city,IP))
+
+def my_city():
+    print('Your city: {0}'.format(city))
     return city
+
+def my_coordinates():
+    print('Your coordinates: {0}'.format(loc))
+    return loc
+
+
