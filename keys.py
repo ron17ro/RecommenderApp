496d11fb7c15639a0700236f5f923ad2fc7b9cfb
Roxana <ron17ro@yahoo.com>
2018-12-05 22:38:56 +0000

    changed keys.py

--- keys.py
+++ keys.py
@@ -2,9 +2,9 @@ from boto.s3.connection import S3Connection
 import os
 
 KEYS = dict([
-	('OpenWeatherMap',  S3Connection(os.environ['OpenWeatherMap'])),
-	('google_API',  S3Connection(os.environ['google_API'])),
-	('mailGun',  S3Connection(os.environ['mailGun'])),
-	('CommentEmail',  S3Connection(os.environ['CommentEmail']))
+	('OpenWeatherMap',  S3Connection(os.environ.get('OpenWeatherMap'))),
+	('google_API',  S3Connection(os.environ.get('google_API'))),
+	('mailGun',  S3Connection(os.environ.get('mailGun'))),
+	('CommentEmail',  S3Connection(os.environ.get('CommentEmail')))
 
 ])
