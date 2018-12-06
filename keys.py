6ef51ea7e039f19e34b657e50555cf12a7bd412d
Roxana <ron17ro@yahoo.com>
2018-12-06 09:47:13 +0000

    fixed the connection

--- keys.py
+++ keys.py
@@ -2,9 +2,9 @@ from boto.s3.connection import S3Connection
 import os
 
 KEYS = dict([
-	('OpenWeatherMap',  S3Connection(os.environ.get('OpenWeatherMap'))),
-	('google_API',  S3Connection(os.environ.get('google_API'))),
-	('mailGun',  S3Connection(os.environ.get('mailGun'))),
-	('CommentEmail',  S3Connection(os.environ.get('CommentEmail')))
+	('OpenWeatherMap',  os.environ.get('OpenWeatherMap')),
+	('google_API',  os.environ.get('google_API')),
+	('mailGun',  os.environ.get('mailGun')),
+	('CommentEmail',  os.environ.get('CommentEmail'))
 
 ])
