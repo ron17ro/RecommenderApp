5f4e053cc9f66823b317d8ef7b4fbf9222b3665b
Uriell <uriell@Paolos-MacBook-Pro.local>
2018-12-09 12:05:21 +0000

    update keys file

--- keys.py
+++ keys.py
@@ -1,10 +1,10 @@
 from boto.s3.connection import S3Connection
 import os
 
-KEYS = dict([
-	('OpenWeatherMap',  os.getenv('OpenWeatherMap')),
-	('google_API',  os.getenv('google_API')),
-	('mailGun',  os.getenv('mailGun')),
-	('CommentEmail',  os.getenv('CommentEmail'))
+KEYS = {
+	'OpenWeatherMap':  os.getenv('OpenWeatherMap'),
+	'google_API':  os.getenv('google_API'),
+	'mailGun':  os.getenv('mailGun'),
+	'CommentEmail':  os.getenv('CommentEmail')
 
-])
+}
