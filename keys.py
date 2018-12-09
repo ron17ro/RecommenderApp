76c8301a2106ab7505b19c5e673bbe54caf01659
Roxana <ron17ro@yahoo.com>
2018-12-09 11:05:33 +0000

    get location

--- keys.py
+++ keys.py
@@ -2,9 +2,9 @@ from boto.s3.connection import S3Connection
 import os
 
 KEYS = dict([
-	('OpenWeatherMap',  os.environ.get('OpenWeatherMap')),
-	('google_API',  os.environ.get('google_API')),
-	('mailGun',  os.environ.get('mailGun')),
-	('CommentEmail',  os.environ.get('CommentEmail'))
+	('OpenWeatherMap',  os.getenv('OpenWeatherMap')),
+	('google_API',  os.getenv('google_API')),
+	('mailGun',  os.getenv('mailGun')),
+	('CommentEmail',  os.getenv('CommentEmail'))
 
 ])
