9d36f935037dce3d2e419f1d45c2424afa1d4393
Roxana <ron17ro@yahoo.com>
2018-12-09 11:14:51 +0000

    get location

--- weather.py
+++ weather.py
@@ -6,7 +6,7 @@ import time
 from keys import KEYS
 # from settings import KEYS
 from location import *
-
+import traceback
 
 # importing the key
 owm = pyowm.OWM(KEYS['OpenWeatherMap'])
@@ -43,24 +43,29 @@ def getWeather():
 		status = w.get_status()
 
 	# lists of weather conditions for sunny and rainy
-	sunny = ["Clear", "Clouds","broken clouds"]
+	sunny = ["Clear", "Clouds", "broken clouds"]
 	rainy = ["Thunderstorm", "Drizzle", "Rain", "Snow"]
 
 	if status in sunny:
 		print("Weather looks good. Let's do something outdoor")
 		return 'outdoor'
-        
+
 	elif status in rainy:
 		print("It doesn't look so nice outside. Let's stay indoor today!")
 		return 'indoor'
-        
+
 	else:
 		print('please choose what type of activity you want')
 		return 'io_activities'
-    	
+
 
 # get the city id
 def city_id():
-    return weather_data["id"]
+	try:
+		w_data = weather_data["id"]
+	except Exception:
+		print('error getting the client ip ')
+		traceback.print_exc()
+	return weather_data["id"]
 
 # getWeather()
